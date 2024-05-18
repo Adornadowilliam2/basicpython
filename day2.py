5a89bc4be4ca729dfea85b003a70f5295ce6b441
William Adornado <adornadowilliam@gmail.com>
2024-05-18 14:14:27 +0800

    add new lesson

---
+++ day2.py
@@ -0,0 +1,56 @@
+#welcome to my python lesson
+
+# how to make a variable
+# you can use like a camelCase
+# or a PascalCase
+# or a snake_case
+
+
+#example of naming a variable using camelCase
+multiPlicaTionVariable = 12
+
+#12 x 12
+answer = multiPlicaTionVariable * multiPlicaTionVariable
+#then print the answer
+print(answer)
+
+ 
+#this is an example of PasCal naming
+PowerOfTwoVariable = 12
+
+#using type you can able defined what is that variable you input
+print(type(PowerOfTwoVariable))
+
+#last one is the snake_one_where_it_more_has_an underscore
+
+my_name_is = "William"
+#you can also change the variable to new one
+#try to change this variable or comment it
+my_name_is = "John"
+print(my_name_is + " B. Adornado Jr.")
+
+
+#====================================================
+
+#you can also make an array of vairable using this
+
+#           0        1        2
+array = "Orange", "Apple", "Grape"
+print(array)
+
+#you can also make call this specific
+# 2  = Grape
+print(array[2])
+
+#also you can do this one
+apple, orange, mango = "Apple","Orange","Mango"
+print(orange)
+print(apple)
+print(mango)
+
+# you can also combine this three
+# without space
+print(orange + apple + mango)
+# with space
+print(orange, apple, mango)
+
