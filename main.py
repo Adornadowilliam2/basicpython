6eb96ae91feddb85449f478ee0b83b1f4a793efa
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-08 13:04:13 +0800

    add simple calculator i learn from the sololearn

---
+++ main.py
@@ -0,0 +1,29 @@
+variable = 5/2
+print(variable)
+
+num = 360
+print(type(num))
+
+
+print("")
+print("Welcome to Python calulator")
+print("Input num1:")
+num1 = int(input())
+print("Input num2:")
+num2 = int(input())
+print("1.) plus \n2.) minus \n3.) times \n4.) divide")
+print("Input operation:")
+
+op = int(input())
+if op == 1:
+    print("Total", num1 + num2)
+elif op == 2:
+    print("Total", num1 - num2)
+elif op == 3:
+    print("Total", num1 * num2)
+elif op == 4: 
+    print("Total", num1 / num2)
+else:
+    print("Invalid number, please try again!")
+    
+
