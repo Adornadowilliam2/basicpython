1b1cff0bfbfa1922f86b4016146ac89783807f03
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-19 14:06:47 +0800

    add something

---
+++ inamo.py
@@ -0,0 +1,20 @@
+colors = ["Red","blue","green","yellow"]
+print(colors[1:-1])
+
+badword = 'motorbike'
+print(badword[-4:])
+
+emoji = ["happy","Sad","Anger","Cry"]
+emoji[1] = "sleepy"
+
+print(emoji)
+
+
+item = ["clothes","hat","shoes","tie"]
+item[:2] = ["bomb","drugs"]
+print(item)
+
+
+vehicle = 'airplane'
+vehicle[:3] = 'water'
+print(vehicle)
