ccf8e6769d833fd90965e5121b9045d33dc663aa
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-25 16:30:44 +0800

    add day4

---
+++ day4.py
@@ -0,0 +1,18 @@
+domain = 'doichi no tenkai'
+print(domain.upper())
+
+gojo = 'gojo satoru'
+print(gojo.replace('gojo satoru', 'yuta okutsu'))
+
+print(domain.capitalize())
+
+print('String are immutable')
+# it means variables are not changeable
+# for example: domain
+
+# the use  of find is to find the index of the string
+print(gojo.find('s'))
+# also find must have always has a value inside the parenthesis else it will throw some error
+
+# the use of split is to split the string
+print(gojo.split(' '))
