7f2002322435c62884b1c7dd813e03dd8285f684
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-26 17:33:15 +0800

    learning about the function

---
+++ day6.py
@@ -0,0 +1,17 @@
+# learning about the function
+def greet():
+    print("hello")
+    print("how are you")
+    print("fine")
+    print("thank you")
+
+greet()
+
+
+def alarm(name):
+    print("Boom", name)
+
+alarm("William")
+alarm("John")
+alarm("Gege")
+# did you store this info??
