6d71c06c0296e736377b8b24da5d275f4690288a
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-26 14:45:22 +0800

    add new learning

---
+++ day5.py
@@ -0,0 +1,21 @@
+fire = 'Igneel'
+
+print(len(fire))
+
+# for adding a new element in the list or array
+dragons = ['Fire', 'Water', 'Earth', 'Air']
+dragons.append('Lightning')
+
+print(dragons)
+
+devil_fruits = ["Gomu gomu","Yami yami","Hito hito"]
+devil_fruits.insert(2,"Kage kage")
+
+print(devil_fruits)
+
+#while the pop will remove the last element of the list
+yonko = ["Luffy","Shanks","Buggy","Blackbeard"]
+
+yonko.pop(2)
+
+print(yonko)
