e262b1cb197eddd5986c432c741f00a668cdf44e
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-08-29 21:42:05 +0800

    add this new learning

---
+++ day7.py
@@ -0,0 +1,15 @@
+year = '1984'
+
+print(len(year))
+
+alien = input("Input your name: ")
+
+alien = alien.upper()
+print("Hello", alien)
+
+if alien == 'WILLIAM':
+    print("YOU GOT THE ALIEN")
+elif alien == 'william':
+    print("YOU GOT THE ALIEN BUT IN LOWERCASE")
+else:
+    print("YOU LOSE THE ALIEN")
